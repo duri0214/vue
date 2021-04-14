404938076d34b42d06c587734be9e30925e962a1
yoshi <yoshitakaOkada0214@gmail.com>
2021-04-14 23:49:39 +0900

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<!-- Header.vue -->
+<template>
+  <header>
+    <router-link to="/">
+      <h1>whisperer.</h1>
+    </router-link>
+    <div class="btns">
+      <button>
+        <fa icon="user" />
+      </button>
+    </div>
+  </header>
+</template>
